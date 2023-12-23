2d483ff0f5b3057afc413fc0cb9cace12c5a6ea4
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-12-23 00:48:19 -0500

    Extract SearchBar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import Image from "next/image";
+import { BusinessLogicContext } from "@/components/contexts/businessLogicContext";
+
+const SearchBar = () => {
+  const { filterByName } = useContext(BusinessLogicContext);
+
+  return (
+    <div className="nav-container-menu-search">
+      <input
+        type="text"
+        onChange={(e) => filterByName(e.target.value)}
+        placeholder="Search a portfolio ..."
+      />
+      <button>
+        <Image
+          src="assets/search-eye-line.svg"
+          alt="search"
+          width={22}
+          height={22}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
