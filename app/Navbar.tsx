a91c99731d5e4f8ed960f7ad270db23fe07fd4b5
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-12-25 03:36:23 -0500

    Set up portfolio page + Update navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,18 +5,24 @@ import { CountryContext } from "@/app/components/contexts/countryContext";
 import { ThemeContext } from "@/app/components/contexts/themeContext";
 import { LogoSvg, ThemeSvg } from "./components/Icons";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
   const { country } = useContext(CountryContext);
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Portfolios", href: "/portfolios" },
+  ];
+  const currentPath = usePathname();
 
   return (
     <>
       <nav className="nav" id="#nav">
-        <div className="nav-container">
-          <div className="nav-container-left">
+        <div className="nav-content">
+          <div className="nav-content-left">
             {country && (
-              <div className="nav-container-left-flag">
+              <div className="nav-content-left-flag">
                 <Image
                   alt={country.name}
                   src={country.flag}
@@ -25,12 +31,23 @@ const NavBar = () => {
                 />
               </div>
             )}
-            <Link href="/public" className="nav-container-left-logo">
+            <Link href="/" className="nav-content-left-logo">
               <LogoSvg />
             </Link>
+            <ul>
+              {links.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className={currentPath === link.href ? "active" : ""}
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </ul>
           </div>
-          <div className="nav-container-menu">
-            <button onClick={toggleTheme} className="nav-container-menu-mode">
+          <div className="nav-content-right">
+            <button onClick={toggleTheme} className="nav-content-right-mode">
               <ThemeSvg theme={theme} />
             </button>
           </div>
